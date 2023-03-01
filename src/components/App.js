3e7b0a4719ecb867910fea5fe51dbb23e78fb11e
Prachi Gaikwad <prachi.gaikwad@tigeranalytics.local>
2023-03-02 00:30:48 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Boost from "./Boost";
+import Advanced from "./Advanced";
+import Showcase from "./Showcase";
+import Shortener from "./Shortner";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Showcase />
+      <Shortener />
+      <Advanced />
+      <Boost />
+      <Footer />
+    </>
+
+
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from '../components/App';
+
+const Home = () => {
+
+    return (
+        <div>
+
+            <App />
+        </div>
+    );
+
+};
+
+export default Home;
